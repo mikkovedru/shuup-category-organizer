2e93be859548aaf58c26746563d47b5fd49b37d9
chessbr <christianhess.rlz@gmail.com>
2018-10-17 11:57:27 -0300

    Initial version
    Refs ENT-2674

--- README.rst
+++ README.rst
@@ -1,2 +1,52 @@
-La Organizer
-============
+Shuup Category Organizer
+========================
+
+Provide a feature to better organize categories.
+
+Shuup is an Open Source E-Commerce Platform based on Django and Python.
+
+https://shuup.com/
+
+Copyright
+---------
+
+Copyright (C) 2012-2018 by Shuup Inc. <support@shuup.com>
+
+Shuup is International Registered Trademark & Property of Shuup Inc.,
+Business Address: 1013 Centre Road, Suite 403-B,
+Wilmington, Delaware 19805,
+United States Of America
+
+CLA
+---
+
+Contributor License Agreement is required for any contribution to this
+project.  Agreement is signed as a part of pull request process.  See
+the CLA.rst file distributed with Shuup.
+
+License
+-------
+
+Shuup Category Organizer is published under Open Software License version 3.0 (OSL-3.0).
+See the LICENSE file distributed with Shuup.
+
+Running tests
+-------------
+
+You can run tests with `py.test <http://pytest.org/>`_.
+
+Requirements for running tests:
+
+* Your virtualenv needs to have Shuup installed.
+
+* Project root must be in the Python path.  This can be done with:
+
+  .. code:: sh
+
+     pip install -e .
+
+To run tests, use command:
+
+.. code:: sh
+
+   py.test -v shuup_category_organizer_tests
---
+++ shuup_category_organizer/admin_module/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2018, Shuup Inc. All rights reserved.
+#
+# This source code is licensed under the OSL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+from django.utils.translation import ugettext_lazy as _
+from shuup.admin.base import AdminModule, MenuEntry
+from shuup.admin.menu import PRODUCTS_MENU_CATEGORY
+from shuup.admin.utils.permissions import get_default_model_permissions
+from shuup.admin.utils.urls import admin_url
+from shuup.core.models import Category
+
+
+class CategoryOrganizerModule(AdminModule):
+    name = _("Categories")
+    category = _("Categories")
+    breadcrumbs_menu_entry = MenuEntry(text=name, url="shuup_admin:category.list", category=PRODUCTS_MENU_CATEGORY)
+
+    def get_urls(self):
+        return [
+            admin_url(
+                "^categories/organize/$",
+                "shuup_category_organizer.admin_module.views.CategoryOrganizeView",
+                name="category.organize",
+                permissions=get_default_model_permissions(Category)
+            ),
+            admin_url(
+                "^categories/(?P<pk>\d+)/duplicate/$",
+                "shuup_category_organizer.admin_module.views.CategoryDuplicateView",
+                name="category.duplicate",
+                permissions=get_default_model_permissions(Category)
+            )
+        ]
+
+    def get_required_permissions(self):
+        return get_default_model_permissions(Category)
