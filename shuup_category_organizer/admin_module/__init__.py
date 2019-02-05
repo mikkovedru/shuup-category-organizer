b14a0243d8b685a0f08854a2c8983df68d995a90
Panu Tulimäki <panu@shuup.com>
2019-02-05 08:18:24 -0800

    Tune admin module name and permissions

--- shuup_category_organizer/admin_module/__init__.py
+++ shuup_category_organizer/admin_module/__init__.py
@@ -8,14 +8,12 @@
 from django.utils.translation import ugettext_lazy as _
 from shuup.admin.base import AdminModule, MenuEntry
 from shuup.admin.menu import PRODUCTS_MENU_CATEGORY
-from shuup.admin.utils.permissions import get_default_model_permissions
 from shuup.admin.utils.urls import admin_url
-from shuup.core.models import Category
 
 
 class CategoryOrganizerModule(AdminModule):
-    name = _("Categories")
-    category = _("Categories")
+    name = _("Category Organizer")
+    category = _("Category Organizer")
     breadcrumbs_menu_entry = MenuEntry(text=name, url="shuup_admin:category.list", category=PRODUCTS_MENU_CATEGORY)
 
     def get_urls(self):
@@ -23,16 +21,11 @@ class CategoryOrganizerModule(AdminModule):
             admin_url(
                 r"^categories/organize/$",
                 "shuup_category_organizer.admin_module.views.CategoryOrganizeView",
-                name="category.organize",
-                permissions=get_default_model_permissions(Category)
+                name="category.organize"
             ),
             admin_url(
                 r"^categories/(?P<pk>\d+)/duplicate/$",
                 "shuup_category_organizer.admin_module.views.CategoryDuplicateView",
-                name="category.duplicate",
-                permissions=get_default_model_permissions(Category)
+                name="category.duplicate"
             )
         ]
-
-    def get_required_permissions(self):
-        return get_default_model_permissions(Category)
